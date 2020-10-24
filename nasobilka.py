cf3ab6fd0d07ab7ed6a8745e79013e79ab015e7c
Ondřej Rousek <ondrey@atlas.cz>
2020-10-24 10:00:34 +0200

    načtení ini jako class

--- nasobilka.py
+++ nasobilka.py
@@ -8,20 +8,37 @@ a_zacina_od = 3    	             # b zacina od 0
 soubor_ini = 'nasobilka.ini'
 fn = datetime.datetime.now().strftime("%m%d%H%M")
 
-try:
-    with open(soubor_ini, "r", encoding="utf-8") as dest:
-        inis = dest.readlines()
-    for vals in inis:
-        val = vals.split('=')
-        if val[0] == 'jmeno':
-            jmeno_uzivatele = val[1]
-    print('spouští uživatel {}'.format(jmeno_uzivatele))
-    pokr = True    
-except:
-    print('soubor {} se jménem uživatele neexistuje'.format(soubor_ini))
-    pokr = False
+class read_ini():
+    """ načtení ini
+    """
+    def __init__(self):
+        try:
+            with open(soubor_ini, "r", encoding="utf-8") as dest:
+                inis = dest.readlines()
+            for vals in inis:
+                val = vals.split('=')
+                if val[0] == 'jmeno':
+                    self.jmeno_uzivatele = val[1]
+            print('spouští uživatel {}'.format(self.jmeno_uzivatele))
+            self.pokr = True    
+        except:
+            print('soubor {} se jménem uživatele neexistuje'.format(soubor_ini))
+            self.pokr = False
 
-while pokr:
+    def pokr():
+        """ načtení ini v pořádku - možno pokračovat ?
+        """
+        return self.pokr
+
+    def jmeno_uzivatele():
+        """ jméno uživatele z ini
+        """
+        return self.jmeno_uzivatele
+
+ini = read_ini()            
+
+
+while ini.pokr:
     try:
         #print('zadejte a: ')
         pom = max_vysledek / 10
@@ -29,13 +46,13 @@ while pokr:
         b = int(random.random()* pom)
 
         if a * b > max_cislo_umi:
-            vysl = '{} ještě neumí {} {} {} = {}'.format(jmeno_uzivatele, a, znak_nasobeni, b, a * b)
+            vysl = '{} ještě neumí {} {} {} = {}'.format(ini.jmeno_uzivatele, a, znak_nasobeni, b, a * b)
             v = 0
         else:
             vysl = 'je to špatně'
             print('zadej výsledek {} {} {}: '.format(a, znak_nasobeni, b))
             v = int(input())
-        if v == a*b:
+        if v == a * b:
             vysl = 'je to dobře'
 
         print('{}'.format(vysl))
